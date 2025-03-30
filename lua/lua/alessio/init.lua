e589e1b24605d675aa36c27748cd29a95675349e
alessio-palumbo <alessio.palumbo4@gmail.com>
2025-03-30 14:11:41 +1100

    Add initial lua config

---
+++ lua/lua/alessio/init.lua
@@ -0,0 +1,6 @@
+-- Define leader key before lazy is called, as this is required.
+vim.g.mapleader = " "
+
+require("alessio.lazy_init")
+require("alessio.set")
+require("alessio.remap")
---
+++ lua/lua/alessio/remap.lua
@@ -0,0 +1,65 @@
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Home shortcuts
+map("i", "jk", "<Esc>", opts)
+map("t", "jk", "<C-\\><C-n>", opts)
+
+-- Navigation shortcuts
+map("i", "jj", "<Esc>o", opts)    -- Insert line below without changing mode
+map("i", "<C-e>", "<C-o>A", opts) -- Go to end of line in insert mode
+map("i", "<A-l>", "<Esc>la", opts) -- Move right in insert mode
+map("n", "j", "gj", opts)         -- Move down without skipping wrapped lines
+map("n", "k", "gk", opts)         -- Move up without skipping wrapped lines
+
+-- Save and Quit shortcuts
+map("i", "<C-s>", "<Esc>:w<CR>", opts)  -- Save
+map("n", "<C-s>", ":w<CR>", opts)       -- Save
+map("n", "<C-q>", ":q<CR>", opts)       -- Quit
+
+-- Shortcuts to edit config files
+map("n", "<leader>cf", ":e $MYVIMRC", opts)
+map("n", "<leader>z", ":e $HOME/.zshrc", opts)
+
+-- Better window navigation
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("i", "<C-h>", "<C-\\><C-N>C-w>h", opts)
+map("i", "<C-l>", "<C-\\><C-N>C-w>l", opts)
+map("i", "<C-j>", "<C-\\><C-N>C-w>j", opts)
+map("i", "<C-k>", "<C-\\><C-N>C-w>k", opts)
+map("t", "<C-h>", "<C-\\><C-N>C-w>h", opts)
+map("t", "<C-l>", "<C-\\><C-N>C-w>l", opts)
+map("t", "<C-j>", "<C-\\><C-N>C-w>j", opts)
+map("t", "<C-k>", "<C-\\><C-N>C-w>k", opts)
+
+map("n", "<leader>v", ":vs<CR>", opts)
+
+-- Resize windows with arrow keys
+map("n", "<A-h>", ":vertical resize -2<CR>", opts)
+map("n", "<A-l>", ":vertical resize +2<CR>", opts)
+
+-- Misc
+map("n", "<CR>", ":nohlsearch", opts)   -- Clear highlights after search with enter
+map("n", "*", ":keepjumps normal! mi*`i<CR>", opts) -- Keeps the cursor under current word when searching
+
+map("n", "<leader>p", "\"0p", opts)     -- Paste last yanked register
+map("v", "<leader>p", "\"0p", opts)     -- Paste last yanked register
+
+map("n", "<leader>n", "o<Esc>", opts)   -- Insert new line below in normal mode
+
+map("n", "<leader>ww", ":FixWhitespace<CR>", opts)   -- Clear whitespace (requires plugin)
+
+
+
+-- Move selected text up/down
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Keep cursor centered when scrolling
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
