2d248c5b78df748d0c1e4b2109bb05b617cac72d
alessio-palumbo <alessio.palumbo4@gmail.com>
2025-05-19 18:20:41 +1000

    Add initial lua setup

---
+++ lua/alessio/plugins/treesitter.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = { "lua", "go", "markdown", "vim", "bash" },
+      highlight = { enable = true },
+      indent = { enable = true },
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "lua",
+	"go",
+        "markdown",
+        "markdown_inline",
+        "vim",
+        "vimdoc",
+        "c",
+        "bash",
+        "json",
+        "javascript",
+        "yaml",
+        "html",
+        "css",
+        "dockerfile",
+        "gitignore",
+        "query",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end
+}
