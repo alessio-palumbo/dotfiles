a4a05b7c19e379599ae9ccddd20e5fe68ccf5346
alessio-palumbo <alessio.palumbo4@gmail.com>
2025-09-02 20:12:19 +1000

    Fix tmux script

--- .tmux/scripts/tmux-jobs.sh
+++ .tmux/scripts/tmux-jobs.sh
@@ -6,8 +6,4 @@ TTY=$(tmux display -p "#{pane_tty}" | cut -d/ -f3)
 # Count stopped jobs (T state) in current tty
 JOBS=$(ps -o stat= -t "$TTY" | grep -c '^T')
 
-if [ "$JOBS" -gt 0 ]; then
-  echo "[Background Jobs :$JOBS]"
-else
-  echo ""
-fi
+echo "[Jobs :$JOBS]"
